831102352659634580e71185a5caeb23d71f271c
lk <2995201114@qq.com>
2023-02-13 17:19:54 +0800

    fix：登录界面初始化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,137 +1,178 @@
 <template>
   <div class="login-wrapper">
-    <el-header class="header">
-      <Logo class="logo" />
-      <LangChange class="lang" color="#fff" />
-    </el-header>
-    <div class="login-container">
-      <div class="login-left hidden-sm-and-down">
-        <div class="login-left-wrap">
-          <img class="img" src="@/assets/login.png" alt="login-bg" />
-          <h2 class="desc">{{ t('login.desc') }}</h2>
-          <p class="tip">{{ t('login.tip') }}</p>
-        </div>
-      </div>
-      <div class="login-form" :class="{ 'is-mobile': isMobile }">
-        <div class="form-warp">
-          <el-tabs v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane :label="t('login.title')" name="first">
-              <LoginForm />
-            </el-tab-pane>
-            <el-tab-pane :label="t('register.title')" name="second">
-              <RegisterForm />
-            </el-tab-pane>
-          </el-tabs>
-        </div>
-      </div>
+    <div class="login-form">
+      <div class="title">单病种科研平台</div>
+      <el-form ref="loginFormRef" :model="loginForm" :rules="rules" status-icon>
+        <el-form-item prop="userName">
+          <el-input v-model="loginForm.userName" placeholder="请输入用户名" clearable size="large">
+            <template #prefix>
+              <svg-icon name="user" size="20px" color="#000000"/>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="department">
+          <el-select style="width: 100%;" v-model="loginForm.department" placeholder="科室-病种" clearable size="large">
+            <template #prefix>
+              <svg-icon name="params" size="20px" />
+            </template>
+            <el-option label="Zone one" value="shanghai" />
+            <el-option label="Zone two" value="beijing" />
+          </el-select>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" show-password placeholder="请输入密码" clearable size="large">
+            <template #prefix>
+              <svg-icon name="password" size="20px" />
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input style="width: 63%" v-model="loginForm.code" placeholder="请输入验证码" clearable size="large">
+            <template #prefix>
+              <svg-icon name="code" size="20px" />
+            </template>
+          </el-input>
+          <div class="login-code">
+            <!-- <img :src="codeUrl" @click="getCode" /> -->
+            图片位置
+          </div>
+        </el-form-item>
+        <el-form-item prop="remember">
+          <div class="password">
+            <el-checkbox v-model="loginForm.remember" label="记住密码"/>
+            <span>忘记密码</span>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button size="large" style="width: 100%;" color="#2879DA" type="primary" @click="submitForm(loginFormRef)">登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { ref, computed } from 'vue';
-  import { useStore } from 'vuex';
-  import LoginForm from 'views/login/comp/LoginForm.vue';
-  import RegisterForm from './comp/RegisterForm.vue';
-  import LangChange from '@/components/LangChange/index.vue';
-  import { useI18n } from 'vue-i18n';
+import { reactive, ref } from 'vue';
 
-  const store = useStore();
-  const { t } = useI18n();
-  const activeName = ref('first');
-  const isMobile = computed(() => {
-    return store.getters['setting/isMobile'];
-  });
+const loginFormRef = ref();
+const loginForm = reactive({
+  userName: '',
+  department: '',
+  password: '',
+  code: '',
+  remember: '',
+})
 
-  const handleClick = (val) => {
-    console.log(val);
-  };
+const rules = reactive({
+  userName: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+  ],
+  department: [
+    { required: true, message: '请选择科室-病种', trigger: 'change' },
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+  ],
+  code: [
+    { required: true, message: '请输入验证码', trigger: 'blur' },
+  ],
+})
+
+const submitForm = async (formEl) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+
+const resetForm = (formEl) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
+
+const options = Array.from({ length: 10000 }).map((_, idx) => ({
+  value: `${idx + 1}`,
+  label: `${idx + 1}`,
+}))
+
 </script>
 
 <style lang="scss" scoped>
   .login-wrapper {
-    position: relative;
-    .header {
-      position: absolute;
-      top: 0;
-      right: 0;
-      left: 0;
-      z-index: 99;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 0 40px;
-      color: #fff;
-      background: transparent;
-      .logo {
-        justify-content: start;
-        :deep(.logo-title) {
-          color: #fff !important;
-        }
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url('@/assets/bg.jpg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    .login-form {
+      width: 400px;
+      height: 470px;
+      padding: 45px 28px;
+      border-radius: 8px;
+      background: #FFFFFF;
+      .title {
+        height: 20px;
+        margin-bottom: 30px;
+        font-size: 20px;
+        font-weight: bold;
+        color: #373E46;
+        text-align: center;
       }
-      .lang:hover {
-        background: transparent;
-      }
-    }
-    .login-container {
-      display: flex;
-      width: 100%;
-      height: 100vh;
-      overflow: hidden;
-      background-color: $dark-bg-color;
-      .login-left {
-        position: relative;
+      .password {
+        width: 100%;
         display: flex;
-        flex-direction: column;
-        width: 50vw;
-        height: 100%;
-        background-image: url('@/assets/login-bg-dark.svg');
-        background-repeat: no-repeat;
-        background-position: 100%;
-        background-size: auto 100%;
-        &-wrap {
-          height: 80vh;
-          margin: auto;
-          .img {
-            width: 280px;
-            margin-top: 10vh;
-          }
-          .title,
-          .desc {
-            max-width: 500px;
-            font-weight: bold;
-            color: #fff;
-            letter-spacing: 1.2px;
-          }
-          .desc {
-            font-size: 28px;
-          }
-          .tip {
-            color: #fff;
-          }
-        }
-      }
-      .login-form {
-        display: flex;
+        justify-content: space-between;
         align-items: center;
-        justify-content: center;
-        width: 50vw;
-        height: 100vh;
-        .form-warp {
-          width: 400px;
-          padding: 1rem 3rem 0 3rem;
-          margin: auto;
-          background-color: #fff;
-          border-radius: 8px;
+        span {
+          font-size: 14px;
+          font-weight: 400;
+          color: #2879DA;
+          cursor: pointer;
         }
-        &.is-mobile {
+      }
+      .login-code {
+        width: 33%;
+        height: 38px;
+        float: right;
+        margin-left: 4%;
+        img {
+          cursor: pointer;
+          vertical-align: middle;
           width: 100%;
-          .form-warp {
-            width: 100%;
-            margin: 0 15px;
-          }
         }
       }
     }
+  }
+
+  .code {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  :deep(.el-checkbox__label) {
+    color: #6A7787;
+  }
+
+  :deep(input::-webkit-input-placeholder) {
+    color:#C7CEDD;
+  }
+  :deep(input::-moz-input-placeholder) {
+    color:#C7CEDD;
+  }
+  :deep(input::-ms-input-placeholder) {
+    color:#C7CEDD;
+  }
+  :deep(input) {
+    color: #6A7787;
   }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -112,7 +112,7 @@ export default defineConfig({
       //   rewrite: (path) => path.replace(new RegExp(`^${import.meta.env.VITE_BASE_API}`), ''),
       // },
       '/dev-api': {
-        target: `http://192.168.0.222:8081`,
+        target: `http://192.168.0.127:8080`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/dev-api/, ""),
       }
